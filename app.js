b5c1584e4cc365d70f4f20b19bb04a5f8d29a2db
Iván Rodríguez <ivan18rt@outlook.com>
2023-06-10 13:33:49 -0600

    proyecto listo

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const paragraph = document.querySelector(".result");
 const url = "https://icanhazdadjoke.com/";
 
 button.addEventListener("click", async () => {
+  fetchJoke();
+});
+
+const fetchJoke = async () => {
+  paragraph.textContent = "Loading..";
   try {
     const response = await fetch(url, {
       headers: {
@@ -10,9 +15,18 @@ button.addEventListener("click", async () => {
         "User-Agent": "Learning app",
       },
     });
+    console.log(response);
+    if (response.ok == false || response.status === 404) {
+      console.log("lol");
+      throw new Error("The url was not correct I guess");
+    }
     const data = await response.json();
+    console.log(data);
     paragraph.textContent = data.joke;
   } catch (error) {
-    console.log(error);
+    console.log(error.message);
+    paragraph.textContent = "There was an error";
   }
-});
+};
+
+fetchJoke();
